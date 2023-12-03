f7356d36b7fbc0c28d5bd3b60ed879898985ae08
shurtandtie <147562046+shurtandtie@users.noreply.github.com>
2023-12-02 22:07:11 -0500

    fixed a competing p5.js issue on the waste page

--- p5js/waste.js
+++ p5js/waste.js
@@ -1,89 +1,84 @@
-let images = []; // store loaded images
-let icon;
-let buttons = [];
-let spacing = 170; // spacing between images
+let timelineSketch = function (p) {
+  let images = [];
+  let icon;
+  let spacing = 170;
 
-function preload() {
-  images[0] = loadImage('images/photo1.png'); // home
-  images[1] = loadImage('images/photo2.png'); // emissions
-  images[2] = loadImage('images/photo3.png'); // energy
-  images[3] = loadImage('images/photo4.png'); // waste
-  images[4] = loadImage('images/photo5.png'); // together
+  p.preload = function () {
+      images[0] = p.loadImage('images/photo1.png'); // home
+      images[1] = p.loadImage('images/photo2.png'); // emissions
+      images[2] = p.loadImage('images/photo3.png'); // energy
+      images[3] = p.loadImage('images/photo4.png'); // waste
+      images[4] = p.loadImage('images/photo5.png'); // together
+      icon = p.loadImage('images/icon.png'); // here marker
+  };
 
-  icon = loadImage('images/icon.png'); // here marker
-}
+  p.setup = function () {
+      let canvas = p.createCanvas(780, 172);
+      canvas.parent('timelinecontainer');
+      p.stroke(255);
+  };
 
-function setup() {
-  let canvas = createCanvas(780, 172);
-  canvas.parent('timelinecontainer');
-  stroke(255);
-}
+  p.draw = function () {
+      p.background(0);
 
-function draw() {
-  background(0);
+      let anyImageHovered = false;
 
-  let anyImageHovered = false;
+      for (let i = 0; i < images.length; i++) {
+          let x = i * spacing;
+          let y = p.height - 100;
 
-  for (let i = 0; i < images.length; i++) {
-    let x = i * spacing;
-    let y = height - 100;
+          if (
+              p.mouseX > x &&
+              p.mouseX < x + 170 &&
+              p.mouseY > y &&
+              p.mouseY < y + 100
+          ) {
+              p.image(images[i], x, y, 100, 100);
+              anyImageHovered = true;
 
-    // hover
-    if (
-      mouseX > x &&
-      mouseX < x + 170 &&
-      mouseY > y &&
-      mouseY < y + 100
-    ) {
-      // marker placement
-      image(images[i], x, y, 100, 100);
-      anyImageHovered = true;
+              let iconX = x + (100 - icon.width * 0.3) / 2;
+              let iconY = y - icon.height * 0.3 - 5;
+              p.image(icon, iconX, iconY, icon.width * 0.3, icon.height * 0.3);
+          } else {
+              p.image(images[i], x, y, 100, 100);
+          }
+      }
 
-      let iconX = x + (100 - icon.width * 0.3) / 2;
-      let iconY = y - icon.height * 0.3 - 5;
-      image(icon, 
-        iconX,iconY, icon.width * 0.3, icon.height * 0.3); // scaled down by 70%
-    
-    } else {
-      // images stay
-      image(images[i], x, y, 100, 100);
-    }
+      if (!anyImageHovered) {
+          let defaultX = 3 * spacing;
+          let defaultY = p.height - 100;
+
+          let iconX = defaultX + (100 - icon.width * 0.3) / 2;
+          let iconY = defaultY - icon.height * 0.3 - 5;
+          p.image(icon, iconX, iconY, icon.width * 0.3, icon.height * 0.3);
+      }
+
+      for (let i = 0; i < images.length - 1; i++) {
+          let centerY = p.height - 50;
+          drawDashedLine(
+              i * spacing + 100,
+              centerY,
+              (i + 1) * spacing,
+              centerY,
+              5
+          );
+      }
+  };
+
+  function drawDashedLine(x1, y1, x2, y2, dashLength) {
+      let dx = x2 - x1;
+      let dy = y2 - y1;
+      let distance = p.sqrt(dx * dx + dy * dy);
+      let segments = distance / dashLength;
+      let deltaX = dx / segments;
+      let deltaY = dy / segments;
+
+      for (let i = 0; i < segments; i += 2) {
+          let dashStartX = x1 + i * deltaX;
+          let dashEndX = p.min(x1 + (i + 1) * deltaX, x2);
+          p.line(dashStartX, y1, dashEndX, y2);
+      }
   }
+};
 
-  // default marker
-  if (!anyImageHovered) {
-    let defaultX = 3 * spacing;
-    let defaultY = height - 100;
-  
-    let iconX = defaultX + (100 - icon.width * 0.3) / 2;
-    let iconY = defaultY - icon.height * 0.3 - 5;
-    image(icon, iconX, iconY, icon.width * 0.3, icon.height * 0.3);
-  }
-
-  // dashed lines
-  for (let i = 0; i < images.length - 1; i++) {
-    let centerY = height - 50; // center y-coordinate of the images
-    drawDashedLine(
-      i * spacing + 100,
-      centerY,
-      (i + 1) * spacing,
-      centerY,
-      5
-    );
-  }
-}
-
-function drawDashedLine(x1, y1, x2, y2, dashLength) {
-  let dx = x2 - x1;
-  let dy = y2 - y1;
-  let distance = sqrt(dx * dx + dy * dy);
-  let segments = distance / dashLength;
-  let deltaX = dx / segments;
-  let deltaY = dy / segments;
-
-  for (let i = 0; i < segments; i += 2) {
-    let dashStartX = x1 + i * deltaX;
-    let dashEndX = min(x1 + (i + 1) * deltaX, x2);
-    line(dashStartX, y1, dashEndX, y2);
-  }
-}
+new p5(timelineSketch);
